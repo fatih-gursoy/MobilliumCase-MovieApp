5fd7d236d342591d548835695ca44e82d40273db
Fatih Gürsoy <fthgrsy@windowslive.com>
2022-11-14 21:50:23 +0300

    Initial Commit

---
+++ MovieApp/Scenes/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Fatih Gursoy on 14.11.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ MovieApp/Scenes/Main/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  MovieApp
+//
+//  Created by Fatih Gursoy on 14.11.2022.
+//
+
+import Foundation
